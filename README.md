197415ce8cc6c406595d43898ddaa3a65e1b3015
vFeesT ;] <43380558+vFeesT@users.noreply.github.com>
2020-10-29 19:51:49 +0100

    repository work in progress, created api for event

---
+++ README.md
@@ -0,0 +1,3 @@
+# event-bus-java
+
+Work in Progress.
---
+++ event-api/src/main/java/io/github/noyzys/facade/eventbus/api/EventBusBootstrapApi.java
@@ -0,0 +1,11 @@
+package io.github.noyzys.facade.eventbus.api;
+
+/*
+  @author: NoyZys on 18:20, 29.10.2020
+ */
+
+/**
+ * An bootstrap cycle of element
+ */
+public interface EventBusBootstrapApi {
+}
